4619c975eeaab0fd1819073c81e5956d70009ab3
felipedelosh <doctorfhernandez@hotmail.com>
2024-11-06 00:44:44 -0500

    FIX: READ not get duplicates examples

--- services/ExampleService.php
+++ services/ExampleService.php
@@ -33,12 +33,24 @@ class ExampleService {
         $example = $this->repository->getByLikeDescription($q);
 
         if($example != null){
-            for ($i = 0; $i < count($example); $i++) { 
-                $data[] = $example[$i]; 
+            for ($i = 0; $i < count($example); $i++) {
+                if (!$this->isExampleInArrayById($data, $example[$i]->id)){
+                    $data[] = $example[$i]; 
+                }
             }
         }
 
         return $data;
+    }
+
+    private function isExampleInArrayById($arr, $id){
+        foreach ($arr as $example) {
+             if ($example->id == $id) {
+                 return true; 
+            } 
+        } 
+        
+        return false;
     }
 
 }
