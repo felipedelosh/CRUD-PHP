a5515b51e63bbd78be783be605f6779d485bbffc
felipedelosh <doctorfhernandez@hotmail.com>
2024-11-05 22:25:43 -0500

    Example model Doc SQL

---
+++ Docs/sql.sql
@@ -0,0 +1,18 @@
+DROP TABLE IF EXISTS examples;
+
+CREATE TABLE examples (
+    id INT PRIMARY KEY,
+    NAME VARCHAR(255),
+    DESCRIPTION TEXT
+);
+
+INSERT INTO examples (id, NAME, DESCRIPTION) VALUES (1, 'Primero', 'Este es el primer ejemplo');
+INSERT INTO examples (id, NAME, DESCRIPTION) VALUES (2, 'Segundo', 'Este es el segundo ejemplo');
+INSERT INTO examples (id, NAME, DESCRIPTION) VALUES (3, 'Tercero', 'Este es el tercer ejemplo');
+INSERT INTO examples (id, NAME, DESCRIPTION) VALUES (4, 'Cuarto', 'Este es el cuarto ejemplo');
+INSERT INTO examples (id, NAME, DESCRIPTION) VALUES (5, 'Quinto', 'Este es el quinto ejemplo');
+INSERT INTO examples (id, NAME, DESCRIPTION) VALUES (6, 'Sexto', 'Este es el sexto ejemplo');
+INSERT INTO examples (id, NAME, DESCRIPTION) VALUES (7, 'Séptimo', 'Este es el séptimo ejemplo');
+INSERT INTO examples (id, NAME, DESCRIPTION) VALUES (8, 'Octavo', 'Este es el octavo ejemplo');
+INSERT INTO examples (id, NAME, DESCRIPTION) VALUES (9, 'Noveno', 'Este es el noveno ejemplo');
+INSERT INTO examples (id, NAME, DESCRIPTION) VALUES (10, 'Décimo', 'Este es el décimo ejemplo');
